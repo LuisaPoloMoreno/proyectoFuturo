b96fc7b06d741d2c158ada1e553c3ea6a6cc43fe
linuxmint20 <linuxmint20@linuxmint20-virtual-machine>
2020-11-15 13:31:01 +0100

    inicio práctica

---
+++ src/proyecto_futuro/Proyecto_Futuro.java
@@ -0,0 +1,34 @@
+package proyecto_futuro;
+
+public class Proyecto_Futuro {
+
+    public static void main(String[] args) {
+
+        int modulos;
+        int horasEstudio;
+        
+        modulos=7;
+        horasEstudio=17;
+
+        String modulosX= String.valueOf(modulos);
+        String horasEstudioX= String.valueOf(horasEstudio);
+        
+        String veredicto= Proyecto_Futuro.veredicto(modulosX, horasEstudioX);
+       
+        System.out.println("Estudiante " + args[0] + " " + args[1] + " " + args[2] + " " + args[3] + ": " + veredicto);
+
+    }
+
+    static String veredicto(String valor1, String valor2) {
+
+        if (valor1.equals(valor2)) {
+            return "Bien. Pero una hora de estudio para cada módulo puede ser insuficiente.";
+        }
+        if (Integer.valueOf(valor1) > Integer.valueOf(valor2)) {
+            return "Poco tiempo de estudio. Debes dedicar más tiempo.";
+        }
+        return "Ideal. Trabajas los contenidos en casa.";
+        
+    }
+
+}
